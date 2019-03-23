d228e149afb3d72f6d955c1ccd47542f13619795
Stjepan Cagalj <stjepan_12@hotmail.com>
2019-03-23 11:28:10 +0100

    Dodan Data Access Layer i ConnectionString

---
+++ FundMe/DAL/FundMeContex.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FundMe.Models;
+using System.Data.Entity;
+
+namespace FundMe.DAL
+{
+    public class FundMeContex : DbContext
+    {
+        public DbSet<Campaign> Campaigns { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Picture> Pictures { get; set; }
+
+    }
+}
