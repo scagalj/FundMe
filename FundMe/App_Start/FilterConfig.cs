cd4a00a0370c764ad1b3438097422d747fffa500
Stjepan Cagalj <stjepan_12@hotmail.com>
2019-03-23 10:28:32 +0100

    Add project files.

---
+++ FundMe/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FundMe
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
