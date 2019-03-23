73ff6d4bb4560803827dff4a311c332ff062b595
Stjepan Cagalj <stjepan_12@hotmail.com>
2019-03-23 11:31:29 +0100

    Dodan kontroler za kampanju s pogledima

---
+++ FundMe/Controllers/CampaignsController.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using FundMe.DAL;
+using FundMe.Models;
+
+namespace FundMe.Controllers
+{
+    public class CampaignsController : Controller
+    {
+        private FundMeContex db = new FundMeContex();
+
+        // GET: Campaigns
+        public ActionResult Index()
+        {
+            var campaigns = db.Campaigns.Include(c => c.Category).Include(c => c.Picture);
+            return View(campaigns.ToList());
+        }
+
+        // GET: Campaigns/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Campaign campaign = db.Campaigns.Find(id);
+            if (campaign == null)
+            {
+                return HttpNotFound();
+            }
+            return View(campaign);
+        }
+
+        // GET: Campaigns/Create
+        public ActionResult Create()
+        {
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Naziv");
+            ViewBag.PictureID = new SelectList(db.Pictures, "ID", "FileName");
+            return View();
+        }
+
+        // POST: Campaigns/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,Description,CampaignsGoal,Date,CategoryID,PictureID")] Campaign campaign)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Campaigns.Add(campaign);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Naziv", campaign.CategoryID);
+            ViewBag.PictureID = new SelectList(db.Pictures, "ID", "FileName", campaign.PictureID);
+            return View(campaign);
+        }
+
+        // GET: Campaigns/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Campaign campaign = db.Campaigns.Find(id);
+            if (campaign == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Naziv", campaign.CategoryID);
+            ViewBag.PictureID = new SelectList(db.Pictures, "ID", "FileName", campaign.PictureID);
+            return View(campaign);
+        }
+
+        // POST: Campaigns/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,Description,CampaignsGoal,Date,CategoryID,PictureID")] Campaign campaign)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(campaign).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "ID", "Naziv", campaign.CategoryID);
+            ViewBag.PictureID = new SelectList(db.Pictures, "ID", "FileName", campaign.PictureID);
+            return View(campaign);
+        }
+
+        // GET: Campaigns/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Campaign campaign = db.Campaigns.Find(id);
+            if (campaign == null)
+            {
+                return HttpNotFound();
+            }
+            return View(campaign);
+        }
+
+        // POST: Campaigns/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Campaign campaign = db.Campaigns.Find(id);
+            db.Campaigns.Remove(campaign);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
