eed3bfa5e1ec43af14eaa63277462b42a03c4cc2
Stjepan Cagalj <stjepan_12@hotmail.com>
2019-03-27 16:50:14 +0100

    Dodan kontroler za Admina te autorizacija

---
+++ FundMe/Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace FundMe.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class AdminController : Controller
+    {
+        // GET: Admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
